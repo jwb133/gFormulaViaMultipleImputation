7fb6c7069aef302fe48b39d5a5a764825846660c
Jonathan Bartlett <jonathan.bartlett1@lshtm.ac.uk>
2024-06-07 09:02:18 +0100

    Revising simulations for paper revisions, stage 1

--- GformulaViaMI_Simulations.R
+++ GformulaViaMI_Simulations.R
@@ -22,8 +22,9 @@ mice.impute.uniABB <- function(y,ry,x = NULL, wy = NULL, ...) {
   sample(x=yry,size=sum(wy),replace=TRUE,prob=probDraw)
 }
 
-#function to perform gformula via MI
-gformulaViaMI <- function(obsData, M=100,l0ABB=FALSE,missingData,maxit=5) {
+#function to perform gformula via MI, increasing M or nSyn if necessary to get a positive variance estimate
+gformulaViaMI <- function(obsData, M=100,nSynMultiplier=1,increaseM=TRUE,
+                          l0ABB=FALSE,missingData,maxit=5) {
   
   n <- nrow(obsData)
   
@@ -35,32 +36,46 @@ gformulaViaMI <- function(obsData, M=100,l0ABB=FALSE,missingData,maxit=5) {
   }
   
   #we repeat until variance estimate is positive
-  #save seed so we can restore if we have to increase number of imps
+  #save seed so we can restore if we have to increase number of imps or nSyn
   startSeed <- .Random.seed
-  currentM <- 0
+  
+  if (increaseM==TRUE) {
+    currentM <- 0
+    currentnSyn <- n*nSynMultiplier
+  } else {
+    #increase nSyn
+    currentM <- M
+    currentnSyn <- 0
+  }
+  
   miVarEst <- 0
   
   while(miVarEst<=0) {
     
-    #add an additional M imputations to what was used previously
-    currentM <- currentM + M
     .Random.seed <- startSeed
+    
+    if (increaseM==TRUE) {
+      #add an additional M imputations to what was used previously
+      currentM <- currentM + M
+    } else {
+      #increase nSyn / nSim
+      currentnSyn <- currentnSyn + n*nSynMultiplier
+    }
 
     if (missingData==TRUE) {
       #use mice to impute missing data
       intermediateImps <- mice(obsData, m=currentM, defaultMethod = c("norm", "logreg", "polyreg", "polr"),
                                printFlag = FALSE, maxit=maxit)
+    } else {
+      #if no missing data, set this object to the observed data
+      intermediateImps <- obsData
+    }
 
-      imps <- gFormulaImpute(intermediateImps,M=currentM,trtVars=c("a0","a1","a2"),
+    #perform G-formula imputation
+    imps <- gFormulaImpute(intermediateImps,M=currentM,nSim=currentnSyn,
+                             trtVars=c("a0","a1","a2"),
                              trtRegimes=list(c(0,0,0),c(1,1,1)),
                              method=methodVal, silent=TRUE)
-      
-    } else {
-      #setup without any regular missing data
-      imps <- gFormulaImpute(obsData,M=currentM,trtVars=c("a0","a1","a2"),
-                             trtRegimes=list(c(0,0,0),c(1,1,1)),
-                              method=methodVal,silent=TRUE)
-    }
     
     #analyse imputed datasets
     fits <- with(imps, lm(y~factor(regime)))
@@ -72,69 +87,217 @@ gformulaViaMI <- function(obsData, M=100,l0ABB=FALSE,missingData,maxit=5) {
     }
   }
     
-  list(miEst=pooled[2,1],miVarEst=pooled[2,4],M=currentM,Bhat=pooled[2,3],Vhat=pooled[2,2])
+  list(miEst=pooled[2,1],miVarEst=pooled[2,4],M=currentM,nSyn=currentnSyn,
+       Bhat=pooled[2,3],Vhat=pooled[2,2])
 }
 
-#function to perform gformula MI simulations
-gformulaMISim <- function(nSim=1000,M=100,n=500, l0ABB=FALSE, 
+#function to perform gformula using gfoRmula
+gfoRmulaRun <- function(obsData, nsimul=NULL) {
+  
+  #first need to reshape to long format
+  longData <- reshape(obsData, direction="long", varying=list(c("a0","a1","a2"),
+                                                              c("l0","l1","l2")),
+                      v.names=c("a","l"),
+                      timevar="time",
+                      times=c(0,1,2))
+  longData <- longData[order(longData$id,longData$t),]
+  #set y to missing at times 0 and 1
+  longData$y[longData$t<2] <- NA
+  
+  id <- 'id'
+  time_name <- 'time'
+  covnames <- c('l', 'a')
+  outcome_name <- 'y'
+  covtypes <- c('normal', 'binary')
+  histories <- c(lagged)
+  histvars <- list(c('a', 'l'))
+  covparams <- list(covmodels = c(l ~ lag1_a + lag1_l + lag2_a + lag2_l + factor(time),
+                                  a ~ lag1_a + lag1_l + lag2_a + lag2_l + factor(time)))
+  ymodel <- y ~ a + lag1_a + lag2_a + l + lag1_l + lag2_l
+  intvars <- list('a', 'a')
+  interventions <- list(list(c(static, rep(0, 3))),
+                        list(c(static, rep(1, 3))))
+  int_descript <- c('Never treat', 'Always treat')
+  
+  #save seed so we can restore random number state afterwards
+  startSeed <- .Random.seed
+  gform_cont_eof <- gformula_continuous_eof(obs_data = longData,
+                                            id = id,
+                                            time_name = time_name,
+                                            covnames = covnames,
+                                            outcome_name = outcome_name,
+                                            covtypes = covtypes,
+                                            covparams = covparams, ymodel = ymodel,
+                                            intvars = intvars,
+                                            interventions = interventions,
+                                            int_descript = int_descript,
+                                            histories = histories, histvars = histvars,
+                                            nsimul = nsimul,
+                                            seed=sample.int(2^30, size = 1),
+                                            model_fits=TRUE)
+  .Random.seed <- startSeed
+  
+  list(gfoRmulaEst = gform_cont_eof$result[3,4]-gform_cont_eof$result[2,4])
+}
+
+simData <- function(n, missingData=FALSE, missingProp=0.5) {
+  
+  #simulate data
+  l0 <- rnorm(n)
+  a0 <- 1*(runif(n)<expit(l0))
+  l1 <- l0+a0+rnorm(n)
+  a1 <- 1*(runif(n)<expit(l1+a0))
+  l2 <- l1+a1+rnorm(n)
+  a2 <- 1*(runif(n)<expit(l2+a1))
+  y <- l2+a2+rnorm(n)
+  
+  obsData <- data.frame(l0=l0,a0=a0,l1=l1,a1=a1,l2=l2,a2=a2,y=y)
+  
+  if (missingData==TRUE) {
+    #make some data missing completely at random
+    obsData$l1[runif(n)<missingProp] <- NA
+    obsData$a1[runif(n)<missingProp] <- NA
+    obsData$l2[runif(n)<missingProp] <- NA
+    obsData$a2[runif(n)<missingProp] <- NA
+    obsData$y[runif(n)<missingProp] <- NA
+  }
+  
+  obsData
+}
+
+#function to perform gformula MI simulations increasing M if needed, nSyn=n
+gformulaMISimIncM <- function(nSim=1000,M=100,n=500,l0ABB=FALSE,
                           missingData=FALSE, missingProp=0.5, progress=TRUE,maxit=5) {
 
-  miEst <- array(0, dim=nSim)
-  miVar <- array(0, dim=nSim)
-  Bhat <- array(0, dim=nSim)
-  Vhat <- array(0, dim=nSim)
-  MRequired <- array(0, dim=nSim)
-  
+  #set up lists/arrays to store results
+  resultList <- list(est=array(0, dim=nSim),
+                        var=array(0, dim=nSim),
+                        Bhat=array(0, dim=nSim),
+                        Vhat=array(0, dim=nSim),
+                        finalM=array(0, dim=nSim),
+                        nSyn=n)
+  #run simulations
   for (sim in 1:nSim) {
     
     if (progress==TRUE) {
       print(sim)
     }
-      
-    l0 <- rnorm(n)
-    a0 <- 1*(runif(n)<expit(l0))
-    l1 <- l0+a0+rnorm(n)
-    a1 <- 1*(runif(n)<expit(l1+a0))
-    l2 <- l1+a1+rnorm(n)
-    a2 <- 1*(runif(n)<expit(l2+a1))
-    y <- l2+a2+rnorm(n)
     
-    obsData <- data.frame(l0=l0,a0=a0,l1=l1,a1=a1,l2=l2,a2=a2,y=y)
+    obsData <- simData(n=n,missingData=missingData,missingProp=missingProp)
     
-    if (missingData==TRUE) {
-      #make some data missing completely at random
-      obsData$l1[runif(n)<missingProp] <- NA
-      obsData$a1[runif(n)<missingProp] <- NA
-      obsData$l2[runif(n)<missingProp] <- NA
-      obsData$a2[runif(n)<missingProp] <- NA
-      obsData$y[runif(n)<missingProp] <- NA
-    }
-    
-    gformMIResult <- gformulaViaMI(obsData=obsData,M=M,l0ABB=l0ABB,
+    #gFormula via MI, increasing M if necessary
+    gformMIResult <- gformulaViaMI(obsData=obsData,M=M,increaseM=TRUE,l0ABB=l0ABB,
                                    missingData=missingData,maxit=maxit)
+    resultList$est[sim] <- gformMIResult$miEst
+    resultList$var[sim] <- gformMIResult$miVarEst
+    resultList$Bhat[sim] <- gformMIResult$Bhat
+    resultList$Vhat[sim] <- gformMIResult$Vhat
+    resultList$finalM[sim] <- gformMIResult$M
     
-    miEst[sim] <- gformMIResult$miEst
-    miVar[sim] <- gformMIResult$miVarEst
-    MRequired[sim] <- gformMIResult$M
-    Bhat[sim] <- gformMIResult$Bhat
-    Vhat[sim] <- gformMIResult$Vhat
-  
   }
   
-  list(miEst=miEst, miVar=miVar, MRequired=MRequired, Bhat=Bhat, Vhat=Vhat)
+  resultList
 
 }
 
-#specify number of simulations to use throughout
-numSims <- 10000
+#function to perform gformula MI simulations increasing nSyn if needed
+gformulaMISimIncnsyn <- function(nSim=1000,M=100,n=500,l0ABB=FALSE,nSynMultiplier=1,
+                              missingData=FALSE, missingProp=0.5, progress=TRUE,maxit=5) {
+  
+  #set up lists/arrays to store results
+  resultList <- list(est=array(0, dim=nSim),
+                     var=array(0, dim=nSim),
+                     Bhat=array(0, dim=nSim),
+                     Vhat=array(0, dim=nSim),
+                     finalnsyn=array(0, dim=nSim),
+                     M=M)
+  #run simulations
+  for (sim in 1:nSim) {
+    
+    if (progress==TRUE) {
+      print(sim)
+    }
+    
+    obsData <- simData(n=n,missingData=missingData,missingProp=missingProp)
+    
+    #gFormula via MI, increasing nSyn if necessary
+    gformMIResult <- gformulaViaMI(obsData=obsData,M=M,increaseM=FALSE,l0ABB=l0ABB,
+                                   nSynMultiplier=nSynMultiplier,
+                                   missingData=missingData,maxit=maxit)
+    resultList$est[sim] <- gformMIResult$miEst
+    resultList$var[sim] <- gformMIResult$miVarEst
+    resultList$Bhat[sim] <- gformMIResult$Bhat
+    resultList$Vhat[sim] <- gformMIResult$Vhat
+    resultList$finalnsyn[sim] <- gformMIResult$nSyn
+    
+  }
+  
+  resultList
+  
+}
 
+#function to perform gformula MI simulations increasing nSyn if needed
+gfoRmulaSim <- function(nSim=1000,nsimul=500,missingData=FALSE,
+                        missingProp=0.5, progress=TRUE) {
+  
+  #set up lists/arrays to store results
+  resultList <- list(est=array(0, dim=nSim))
+  
+  #run simulations
+  for (sim in 1:nSim) {
+    
+    if (progress==TRUE) {
+      print(sim)
+    }
+    
+    obsData <- simData(n=n,missingData=missingData,missingProp=missingProp)
+    
+    #gfoRmula package  
+    gfoRmulaResult <- gfoRmulaRun(obsData=obsData, nsimul=gfoRmulansimul)
+    resultList$est[sim] <- as.numeric(gfoRmulaResult$gfoRmulaEst)
+    
+  }
+  
+  resultList
+  
+}
+
+####################################################################
+# run simulations
+####################################################################
+
+#specify number of simulations to use throughout
+numSims <- 10
+
+#GFormulaMI, no missing data, increasing M if needed, different initial M
 set.seed(738355)
 mVals <- c(5,10,25,50,100)
 mSims <- vector("list", length(mVals))
 for (i in 1:length(mVals)) {
   print(paste("M=",mVals[i],sep=""))
-  mSims[[i]] <- gformulaMISim(nSim=numSims,M=mVals[i],progress=FALSE)
+  mSims[[i]] <- gformulaMISimIncM(nSim=numSims,M=mVals[i],progress=FALSE)
 }
+
+#GFormulaMI, no missing data, increasing nSyn if needed, different initial nSyn
+set.seed(82621)
+nSynMultVals <- c(1,2,5,10)
+nSynSims <- vector("list", length(nSynMultVals))
+for (i in 1:length(nSynMultVals)) {
+  print(paste("nSyn mult=",nSynMultVals[i],sep=""))
+  nSynSims[[i]] <- gformulaMISimIncnsyn(nSim=numSims,nSynMultiplier=nSynMultVals[i],
+                                        M=10,progress=FALSE)
+}
+
+#gfoRmula, no missing data
+set.seed(8946565)
+nsimulVals <- c(500,1000,2000,5000)
+nSynSims <- vector("list", length(nSynMultVals))
+for (i in 1:length(nSynMultVals)) {
+  print(paste("nSyn mult=",nSynMultVals[i],sep=""))
+  nSynSims[[i]] <- gformulaMISimIncnsyn(nSim=numSims,nSynMultiplier=nSynMultVals[i],
+                                        M=10,progress=FALSE)
+}
+
 
 resTable <- array(0, dim=c(length(mVals),8))
 for (i in 1:length(mVals)) {
